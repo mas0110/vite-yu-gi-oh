1384252f6953faa674b4218216d1fd47247df3b8
masoud mohammed <115033795+mas0110@users.noreply.github.com>
2024-02-09 13:47:15 +0100

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,10 @@
+// import './assets/main.css'
+
+import './styles/general.scss'
+
+import * as bootstrap from 'bootstrap'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
